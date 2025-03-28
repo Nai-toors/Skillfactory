e0b7a846e27f05f0f395f3dd3d85d1ede776d26b
Pavel Bibikov <mesoulte@gmail.com>
2025-03-29 00:45:53 +0300

    Сделал модуль 6

--- script.js
+++ script.js
@@ -1,30 +1,70 @@
-// Пример данных
-const person = {
-    name: 'Иван',
-    age: 30,
-    city: 'Москва'
-};
+// Задание 6.6.1
+const tenet = "Довод"; // Сантимент
+let count = 0;
+let answer = "";
 
-const fruits = ['яблоко', 'банан', 'апельсин'];
+const arr1 = Array.from(tenet.toLocaleLowerCase());
 
-// Функция для вывода данных на страницу
-function displayData() {
-    const outputDiv = document.getElementById('output');
-    
-    // Вывод данных об объекте
-    outputDiv.innerHTML += '<h2>Данные о человеке:</h2>';
-    outputDiv.innerHTML += `<p>Имя: ${person.name}</p>`;
-    outputDiv.innerHTML += `<p>Возраст: ${person.age}</p>`;
-    outputDiv.innerHTML += `<p>Город: ${person.city}</p>`;
-
-    // Вывод данных из массива
-    outputDiv.innerHTML += '<h2>Список фруктов:</h2>';
-    outputDiv.innerHTML += '<ul>';
-    fruits.forEach(fruit => {
-        outputDiv.innerHTML += `<li>${fruit}</li>`;
-    });
-    outputDiv.innerHTML += '</ul>';
+for (let i = 0; i < arr1.length; i += 1) {
+  if (arr1[i] === arr1[arr1.length - i - 1]) {
+    count += 1;
+  }
+}
+if (count === arr1.length) {
+  answer += `Слово ${tenet} является паиндромом`;
+} else {
+  answer += `Слово ${tenet} не является палиндромом`;
 }
 
-// Вызов функции при загрузке страницы
-window.onload = displayData;
+function displayData(answer, elementId) {
+  const outputDiv = document.getElementById(elementId);
+  outputDiv.innerHTML += answer;
+}
+displayData(answer, "output");
+
+// Задание 6.6.2
+const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
+
+const unique = Array.from(new Set(arr)).join(', ');
+unique.slice(0, unique.length); // для удаления последнего пробела, хоть он и не показывается
+
+displayData(unique, "output2");
+
+// Задание 6.6.3
+const userInput = prompt("Введите любое число:");
+const num = Number(userInput);
+
+let numberArray = [];
+for (let i = 0; i <= num; i++) {
+  numberArray.push(i);
+}
+
+displayData(numberArray, "output3");
+
+// Задание 6.6.4
+for (let i = 0; i < 3; i += 1) {
+  if (i % 2 === 0) {
+    console.log('X O X');
+  } else {
+    console.log('O X O');
+  }
+}
+
+// Задание 6.6.5
+const obj = {
+  some: 'some',
+  dom: 'text',
+  arr: [1, 2, 3, 4, 5],
+  tom: 'there'
+};
+
+const arrValues = [];
+
+for (var key in obj)  { 
+  arrValues.push(obj[key]);
+}
+
+displayData(arrValues, "output4");
+
+var arrValues1 = [].concat(obj.some, obj.dom, obj.arr, obj.tom);
+console.log(arrValues1);
